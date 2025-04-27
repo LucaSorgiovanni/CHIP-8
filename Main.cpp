ea8d93ae67241ec1e062884dca63049afee6a059
luca sorgiovanni <sorgiovanniluca40@gmail.com>
2025-04-27 18:15:14 +0200

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,679 @@
+#include <iostream>
+#include <stdint.h>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+#include <time.h> 
+#include <SDL3/SDL_main.h>
+#include <SDL3/SDL_timer.h>
+
+
+enum RegistersNames
+{
+	VO, V1, V2, V3, V4, V5, V6, V7, V8, V9, VA, VB, VC, VD, VE, VF
+};
+
+enum MemoryMap
+{
+	PROGRAM_START = 0x200,
+	FONT_START = 0x50,
+	FONT_END = 0x9F,
+};
+
+enum ShifMode
+{
+	COSMAC_VIP,
+	SUPER_CHIP,
+	UNKNOWN,
+};
+
+SDL_Scancode chip8_keymap[16] = {
+	SDL_SCANCODE_X, // 0
+	SDL_SCANCODE_1, // 1
+	SDL_SCANCODE_2, // 2
+	SDL_SCANCODE_3, // 3
+	SDL_SCANCODE_Q, // 4
+	SDL_SCANCODE_W, // 5
+	SDL_SCANCODE_E, // 6
+	SDL_SCANCODE_A, // 7
+	SDL_SCANCODE_S, // 8
+	SDL_SCANCODE_D, // 9
+	SDL_SCANCODE_Z, // A
+	SDL_SCANCODE_C, // B
+	SDL_SCANCODE_4, // C
+	SDL_SCANCODE_R, // D
+	SDL_SCANCODE_F, // E
+	SDL_SCANCODE_V  // F
+};
+
+uint16_t opcode{};
+uint8_t memory[4096] {};
+uint16_t programCounter{};
+uint16_t indexRegister {};
+std::vector<uint16_t> stack {};
+uint8_t delayTimer {};
+uint8_t soundTimer{};
+uint8_t registers[16]{};
+uint32_t display[64 * 32]{};
+
+uint8_t font[80]{ 0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+				  0x20, 0x60, 0x20, 0x20, 0x70, // 1
+				  0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+				  0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+				  0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+				  0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+			      0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+				  0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+				  0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+				  0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+				  0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+				  0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+				  0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+				  0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+				  0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+				  0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
+
+uint64_t NSperFrame { 1'000'000'000 / 60 };
+
+bool keypad[16]{}; 
+bool isKeyPressed{};
+uint8_t keyPressed{};
+bool isKeyReleased{};
+
+extern SDL_Window* gWindow;
+
+bool init();
+void update();
+void clearScreen();
+void close();
+
+uint8_t draw(uint8_t x, uint8_t y, uint8_t N, uint16_t index);
+
+void LoadFont() {
+
+	for (int i = 0; i < 80; i++)
+	{
+		memory[FONT_START + i] = font[i];
+	}
+}
+
+bool OpenROM(const std::string& pathROM) {
+
+	std::ifstream ROMfile{pathROM, std::ios::binary | std::ios::ate };
+
+	if (ROMfile.is_open())
+	{
+		int size{ static_cast<int>(ROMfile.tellg()) };
+		char* buffer = new char[size];
+
+		ROMfile.seekg(0, std::ios::beg);
+		ROMfile.read(buffer, size);
+
+		for (int i = 0; i < size; i++)
+		{
+			memory[PROGRAM_START + i] = buffer[i];
+		}
+		programCounter = PROGRAM_START;
+		return true;
+	}
+
+	return false;
+}
+
+void fetch() {
+	opcode = memory[programCounter];
+	opcode = opcode << 8;
+	opcode = opcode | memory[programCounter + 1];
+	programCounter += 2;
+}
+
+void OPC0x00E0() {
+	clearScreen();
+}
+
+void OPC0x00EE() {
+	programCounter = stack.back();
+	stack.pop_back();
+}
+
+void OPC0x2NNN(uint16_t OPC) {
+	stack.push_back(programCounter);
+	programCounter = OPC & 0x0FFF;
+
+}
+
+void OPC0x1(uint16_t OPC) {
+	programCounter = OPC & 0x0FFF;
+}
+
+void OPC0x3XNN(uint16_t OPC) {
+
+	uint16_t tempIndex = (OPC & 0x0F00) >> 8;
+	bool condition = (registers[tempIndex] == (OPC & 0x00FF));
+	if (condition)
+	{
+		programCounter += 2;
+	}
+
+}
+
+void OPC0x4XNN(uint16_t OPC) {
+	uint16_t tempIndex = (OPC & 0x0F00) >> 8;
+	bool condition = (registers[tempIndex] != (OPC & 0x00FF));
+	if (condition)
+	{
+		programCounter += 2;
+	}
+}
+
+void OPC0x5XY0(uint16_t OPC) {
+	uint16_t tempXIndex = (OPC & 0x0F00) >> 8;
+	uint16_t tempYIndex = (OPC & 0x00F0) >> 4;
+
+	if (registers[tempXIndex] == registers[tempYIndex])
+	{
+		programCounter += 2;
+	}
+
+}
+
+void OPC0x9XY0(uint16_t OPC) {
+	uint16_t tempXIndex = (OPC & 0x0F00) >> 8;
+	uint16_t tempYIndex = (OPC & 0x00F0) >> 4;
+
+	if (registers[tempXIndex] != registers[tempYIndex])
+	{
+		programCounter += 2;
+	}
+
+}
+
+void OPC0x6(uint16_t OPC) {
+	registers[((OPC & 0x0F00) >> 8)] = (OPC & 0x00FF);
+}
+
+void OPC0x7(uint16_t OPC) {
+	registers[((OPC & 0xF00) >> 8)] += (OPC & 0x00FF);
+}
+
+void OPC0x8XY0(uint16_t OPC) {
+	uint16_t tempXIndex = (OPC & 0x0F00) >> 8;
+	uint16_t tempYIndex = (OPC & 0x00F0) >> 4;
+	registers[tempXIndex] = registers[tempYIndex];
+}
+
+void OPC0x8XY1(uint16_t OPC) {
+	uint16_t tempXIndex = (OPC & 0x0F00) >> 8;
+	uint16_t tempYIndex = (OPC & 0x00F0) >> 4;
+	registers[tempXIndex] = registers[tempXIndex] | registers[tempYIndex];
+}
+
+void OPC0x8XY2(uint16_t OPC) {
+	uint16_t tempXIndex = (OPC & 0x0F00) >> 8;
+	uint16_t tempYIndex = (OPC & 0x00F0) >> 4;
+	registers[tempXIndex] = registers[tempXIndex] & registers[tempYIndex];
+}
+
+void OPC0x8XY3(uint16_t OPC) {
+	uint16_t tempXIndex = (OPC & 0x0F00) >> 8;
+	uint16_t tempYIndex = (OPC & 0x00F0) >> 4;
+	registers[tempXIndex] = registers[tempXIndex] ^ registers[tempYIndex];
+}
+
+void OPC0x8XY4(uint16_t OPC) {
+	uint16_t tempXIndex = (OPC & 0x0F00) >> 8;
+	uint16_t tempYIndex = (OPC & 0x00F0) >> 4;
+
+	bool overflow = registers[tempXIndex] + registers[tempYIndex] > 255;
+
+	registers[tempXIndex] = registers[tempXIndex] + registers[tempYIndex];
+
+	registers[VF] = 0x0;
+
+	if (overflow)
+	{
+		registers[VF] = 0x1;
+	}
+}
+
+void OPC0x8XY5(uint16_t OPC) {
+	uint16_t tempXIndex = (OPC & 0x0F00) >> 8;
+	uint16_t tempYIndex = (OPC & 0x00F0) >> 4;
+
+	bool underflow = registers[tempXIndex] < registers[tempYIndex];
+	registers[tempXIndex] = registers[tempXIndex] - registers[tempYIndex];
+
+	if (underflow)
+	{
+		registers[VF] = 0x0;
+	}
+	else {
+		registers[VF] = 0x1;
+	}
+
+	
+}
+
+void OPC0x8XY7(uint16_t OPC) {
+	uint16_t tempXIndex = (OPC & 0x0F00) >> 8;
+	uint16_t tempYIndex = (OPC & 0x00F0) >> 4;
+
+	bool underflow = registers[tempYIndex] < registers[tempXIndex];
+	registers[tempXIndex] = registers[tempYIndex] - registers[tempXIndex];
+
+	if (underflow)
+	{
+		registers[VF] = 0x0;
+	}
+	else {
+		registers[VF] = 0x1;
+	}
+
+}
+
+void OPC0x8XY6(uint16_t OPC, ShifMode shiftmode ) {
+
+	uint16_t XIndex = (OPC & 0x0F00) >> 8 ;
+	uint16_t Yindex = (OPC & 0x00F0) >> 4 ;
+
+
+	if (shiftmode == COSMAC_VIP)
+	{
+		registers[XIndex] = registers[Yindex];
+	}
+
+	uint16_t shiftvalue = registers[XIndex] & 0x01;
+
+	registers[XIndex] = registers[XIndex] >> 1;
+
+	if (shiftvalue == 1)
+	{
+		registers[VF] = 0x1;
+	}
+	else
+	{
+		registers[VF] = 0x0;
+	}
+	
+}
+
+void OPC0x8XYE(uint16_t OPC, ShifMode shiftmode) {
+
+	uint16_t XIndex = (OPC & 0x0F00) >> 8;
+	uint16_t Yindex = (OPC & 0x00F0) >> 4;
+	uint16_t valueX = registers[XIndex];
+	uint16_t valueY = registers[Yindex];
+
+	if (shiftmode == COSMAC_VIP)
+	{
+		registers[XIndex] = registers[Yindex];
+	}
+
+	uint16_t shiftvalue = ((registers[XIndex] >> 7) & 0x1);
+
+	registers[XIndex] = registers[XIndex] << 1;
+
+	if (shiftvalue == 1)
+	{
+		registers[VF] = 0x1;
+	}
+	else
+	{
+		registers[VF] = 0x0;
+	}
+
+	
+}
+
+void OPC0xA(uint16_t OPC) {
+	indexRegister = OPC & 0x0FFF;
+}
+
+void OPC0xB(uint16_t OPC, ShifMode shiftmode) {
+	
+	uint16_t NNN = OPC & 0x0FFF;
+
+	if (shiftmode == COSMAC_VIP)
+	{
+		programCounter = NNN + registers[0];
+	}
+	else {
+		uint16_t XIndex =  OPC & 0x0F00 >> 4 ;
+		programCounter = NNN + registers[XIndex];
+	}
+}
+
+void OPC0xC(uint16_t OPC) {
+
+	uint16_t XIndex= (OPC & 0x0F00) >> 8 ;
+	uint16_t NN= OPC & 0x00FF ;
+	registers[XIndex] = (rand() % 256) & NN;
+}
+
+void OPC0xD(uint16_t OPC) {
+	//coordinate
+	uint8_t x = registers[((OPC & 0x0F00) >> 8)];
+	uint8_t y = registers[((OPC & 0x00F0) >> 4)];
+	uint16_t x16 = x;
+	uint16_t y16 = y;
+	uint8_t n = OPC & 0x000F; //spight height
+	std::cout << "drawing at: " << std::dec << static_cast<int>(x) << " & " << static_cast<int>(y) << " for " << static_cast<int>(n) << " rows!\n";
+	int VFvalue = draw(x, y, n, indexRegister);
+	registers[VF] = VFvalue;
+	
+}
+
+void OPC0xEX9E(uint16_t OPC) {
+	uint16_t XIndex = (OPC & 0x0F00) >> 8;
+	uint16_t key = registers[XIndex] % 16;
+	if (keypad[key] == true) {
+		programCounter += 2;
+	}
+}
+
+void OPC0xEXA1(uint16_t OPC) {
+	uint16_t XIndex = (OPC & 0x0F00) >> 8;
+	uint16_t key = registers[XIndex] % 16;
+	if (keypad[key] == false) {
+		programCounter += 2;
+	}
+}
+
+void OPC0xFX07(uint16_t OPC) {
+	uint16_t XIndex = (OPC & 0x0F00) >> 8;
+	registers[XIndex] = delayTimer;
+}
+
+void OPC0xFX15(uint16_t OPC) {
+	uint16_t XIndex = (OPC & 0x0F00) >> 8;
+	delayTimer = registers[XIndex];
+}
+
+void OPC0xFX18(uint16_t OPC) {
+	uint16_t XIndex = (OPC & 0x0F00) >> 8;
+	soundTimer = registers[XIndex];
+}
+
+void OPC0xFX1E(uint16_t OPC) {
+	uint16_t XIndex = (OPC & 0x0F00) >> 8;
+	indexRegister += registers[XIndex];
+}
+
+void OPC0xFX0A(uint16_t OPC) {
+	uint16_t XIndex = (OPC & 0x0F00) >> 8;
+	if (isKeyReleased != true)
+	{
+		programCounter -= 2;
+	}
+	else {
+		registers[XIndex] = keyPressed;
+	}
+}
+
+void OPC0xFX33(uint16_t OPC) {
+
+	uint16_t XIndex = (OPC & 0x0F00) >> 8;
+	uint8_t value = registers[XIndex];
+
+	memory[indexRegister + 2]= value % 10;
+	value = value / 10;
+
+	memory[indexRegister + 1] = value % 10;
+	value = value / 10;
+
+	memory[indexRegister] = value % 10;
+}
+
+void OPC0xFX55(uint16_t OPC) {
+	uint16_t XIndex = (OPC & 0x0F00) >> 8;
+
+	for (int i = 0; i <= XIndex; i++)
+	{
+		memory[indexRegister + i] = registers[i];
+	}
+}
+
+void OPC0xFX65(uint16_t OPC) {
+	uint16_t XIndex = (OPC & 0x0F00) >> 8;
+
+	for (int i = 0; i <= XIndex; i++)
+	{
+		 registers[i] = memory[indexRegister + i];
+	}
+}
+
+void decode(uint16_t OPC) {
+
+	if (OPC == 0x00E0)
+	{
+		//clear display
+		std::cout << "display cleared\n";
+	}
+
+
+	switch (((OPC & 0xF000) >> 12))
+	{
+		case 0x0:
+			switch (OPC)
+			{
+				case 0x00E0: // clear screen
+					OPC0x00E0();
+						break;
+				case 0x00EE: //return from subroutine
+					OPC0x00EE();
+					break;
+				default:
+					break;
+			}
+			break;
+		case 0x1: //Jump
+			OPC0x1(OPC);
+			break;
+		case 0x2: //Unconditional Jump
+			OPC0x2NNN(OPC);
+			break;
+		case 0x3: //conditional jump
+			OPC0x3XNN(OPC);
+			break;
+		case 0x4: //conditional jump
+			OPC0x4XNN(OPC);
+			break;
+		case 0x5: //conditional jump
+			OPC0x5XY0(OPC);
+			break;
+		case 0x9: //conditional jump
+			OPC0x9XY0(OPC);
+			break;
+		case 0x6: //register set
+			OPC0x6(OPC);
+			break;
+		case 0x7: //register add (no carry)
+			OPC0x7(OPC);
+			break;
+		case 0x8:
+			switch ((OPC & 0x000F))
+			{
+			case 0x0000:
+				OPC0x8XY0(OPC);
+				break;
+			case 0x0001:
+				OPC0x8XY1(OPC);
+				break;
+			case 0x0002:
+				OPC0x8XY2(OPC);
+				break;
+			case 0x0003:
+				OPC0x8XY3(OPC);
+				break;
+			case 0x0004:
+				OPC0x8XY4(OPC);
+				break;
+			case 0x0005:
+				OPC0x8XY5(OPC);
+				break;
+			case 0x0006:
+				OPC0x8XY6(OPC, COSMAC_VIP);
+				break;
+			case 0x0007:
+				OPC0x8XY7(OPC);
+				break;
+			case 0x000E:
+				OPC0x8XYE(OPC, COSMAC_VIP);
+				break;
+			default:
+				break;
+			}
+			break;
+		case 0xA: //index register set
+			OPC0xA(OPC);
+			break;
+		case 0xD: //display
+			OPC0xD(OPC);
+			break;
+		case 0xE:
+			switch (OPC & 0x00FF)
+			{
+			case 0x009E:
+				OPC0xEX9E(OPC);
+				break;
+			case 0x00A1:
+				OPC0xEXA1(OPC);
+				break;
+			default:
+				break;
+			}
+			break;
+		case 0xF:
+			switch ((OPC & 0x00FF))
+			{
+			case 0x0007:
+				OPC0xFX07(OPC);
+				break;
+			case 0x0015:
+				OPC0xFX15(OPC);
+				break;
+			case 0x0018:
+				OPC0xFX18(OPC);
+				break;
+			case 0x001E:
+				OPC0xFX1E(OPC);
+				break;
+			case 0x000A:
+				OPC0xFX0A(OPC);
+				break;
+			case 0x0033:
+				OPC0xFX33(OPC);
+				break;
+			case 0x0055:
+				OPC0xFX55(OPC);
+				break;
+			case 0x0065:
+				OPC0xFX65(OPC);
+				break;
+			default:
+				break;
+			}
+			break;
+		default:
+			break;
+			break;
+	}
+}
+
+int main(int argc, char* args[]) {
+
+	srand(time(NULL));
+	const std::string ROMpath { "C:/Users/sorgi/Desktop/CH8ROMS/5-quirks.ch8" };
+	LoadFont();
+
+	if (!OpenROM(ROMpath)) {
+		std::cout << "problems opening the ROM! Retry";
+		return 0;
+	}
+	std::cout << "\n";
+
+	if (!init()) {
+		std::cout << "problems initializing SDL! Retry";
+	}
+	else {
+
+		bool quit{ false };
+		SDL_Event e;
+		SDL_zero(e);
+
+		while (!quit)
+		{
+			uint64_t loopStartNS = SDL_GetTicksNS();
+
+			const bool* keyStates = SDL_GetKeyboardState(nullptr);
+
+			for (int i = 0; i < 16; i++)
+			{
+				keypad[i] = keyStates[chip8_keymap[i]];
+
+				if (keypad[i] == true && isKeyPressed != true)
+				{
+					isKeyPressed = true;
+					keyPressed = i;
+					isKeyReleased = false;
+				}
+
+				if (isKeyPressed == true && keypad[keyPressed] == false && isKeyReleased != true)
+				{
+					isKeyReleased = true;
+					isKeyPressed = false;
+				}
+			}
+
+			
+			while (SDL_PollEvent(&e)) {
+
+				if (e.type == SDL_EVENT_QUIT)
+				{
+					quit = true;
+				}
+
+				if (e.type == SDL_EVENT_KEY_DOWN) {
+
+				}
+			}
+
+
+
+			if (delayTimer > 0)
+			{
+				delayTimer--;
+			}
+			
+			if (soundTimer > 0)
+			{
+				soundTimer--;
+			}
+
+			// 11 loop
+			for (int i = 0; i < 11; i++)
+			{
+				fetch();
+				decode(opcode);
+			}
+
+			isKeyReleased = false;
+
+			update();
+
+			uint64_t loopTimeNS{ SDL_GetTicksNS() - loopStartNS };
+
+			if (loopTimeNS < NSperFrame)
+			{
+				SDL_DelayNS(NSperFrame - loopTimeNS);
+			}
+
+		}
+	}
+
+	close();
+	return 0;
+}
+
---
+++ Video.cpp
@@ -0,0 +1,164 @@
+#include <SDL.h>
+#include <iostream>
+#include <stdint.h>
+
+constexpr int windowWidth{ 640 };
+constexpr int windowHeight{ 320 };
+
+extern uint32_t display[64 * 32];
+extern uint8_t memory[4096];
+
+SDL_Window* gWindow{ nullptr };
+
+SDL_Surface* gSurface{ nullptr };
+
+
+bool init() {
+
+	bool success{ true };
+	if (!SDL_Init(SDL_INIT_VIDEO))
+	{
+		SDL_Log("Couldn't init the device");
+		success = false;
+	}
+	else {
+		gWindow = SDL_CreateWindow("CHIP-8", windowWidth, windowHeight, 0);
+
+		if (gWindow == nullptr)
+		{
+			SDL_Log("Couldn't create the Window");
+			success = false;
+		}
+		else {
+			gSurface = SDL_GetWindowSurface(gWindow);
+		}
+	}
+
+	SDL_FillSurfaceRect(gSurface, nullptr, SDL_MapSurfaceRGB(gSurface, 0x00, 0x00, 0x00));
+	return true;
+}
+void update() {
+	
+	SDL_UpdateWindowSurface(gWindow);
+}
+
+void clearScreen() {
+	SDL_FillSurfaceRect(gSurface, nullptr, SDL_MapSurfaceRGB(gSurface, 0x00, 0x00, 0x00));
+}
+
+void close() {
+
+	SDL_DestroyWindow(gWindow);
+	gWindow = nullptr;
+	gSurface = nullptr;
+	SDL_Quit();
+}
+
+/*
+voglio vedere i cambiamenti frame per frame uno alla volta quindi aspetto cosi
+se volessi vedere tutta l'immagine cambiare mi converrebbe prendere le informazioni del display e modificarle sotto
+(penso?)
+*/
+void drawScaledPixel(int x, int y, bool white) {
+
+	uint32_t* Pixels = (uint32_t*)gSurface->pixels;
+	uint32_t color{};
+
+	int pitch = gSurface->pitch / 4;
+
+	if (white == false) //bianco su nero quindi quando bianco è falso.  Se bianco è vero coloro i 10 pixel da colorare in bianco e quindi li riempio
+	{
+		color = SDL_MapRGB(SDL_GetPixelFormatDetails(gSurface->format), nullptr, 0xFF, 0xFF, 0xFF);
+	}
+	else {
+		color = SDL_MapRGB(SDL_GetPixelFormatDetails(gSurface->format), nullptr, 0x00, 0x00, 0x00);
+	}
+	
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			Pixels[((y) + i) * pitch + ((x) + j)] = color;
+			
+		}
+	}
+}
+
+uint8_t draw(uint8_t x, uint8_t y, uint8_t N, uint16_t index) {
+
+	uint8_t VF{0}; //valore da ritornare in caso di collisione: 0 se ho semplicemento acceso il pixel, 1 se il pixel l'ho spento
+	//lo metto 0 ora e poi lo cambio in caso serve cambiarlo dopo sennò rimane 0 e tutti felici :)
+
+
+	for (int row = 0; row < N; row++) // le righe sono definite dal comandy DXYN e decide l'altezza di quello che va disegnato
+	{   // ogni porcoddio di disegno è definito in una successione di memoria lunga quanto è alto il disegno quindi se N = 3 la prima riga sara un numero esadecimale da 8 bit Z il secondo V e così via e sti numeri si trovano nella memoria della "cassetta" ( non so che cazzo usavano ai tempi quindi la chiamo cassetta)
+		
+		if (row + y > 32) // se lo sprite va fuori dallo schermo in verticale smetto di disegnare
+		{ // row + x sarebbe per controllare se posso disegnare la riga corrente (se inizio a disegnare a 30 per 4 righe e arrivo al ciclio in cui voglio disegnare alla riga 30 + 3 non posso perche ne ho solo 32
+			break; // qua break perchè se sono fuori riga non posso andare a destra della colonna
+		}
+
+		uint8_t spriteByte = memory[index + row]; //qua prendo la prima riga dello sprite che è semplicemente un numero in esadecimale da 8 bit (esempio riga sotto)
+												  //lo sprite di e0 è: 1110 0000 dove gli 1 sono i pixel che cambiano stato e gli 0 fregacazzi
+		for (int col = 0; col < 8; col++) { //8 colonne 8 bit (se non capisci sei ritardato)
+
+			if (col + x > 64) // se lo sprite va fuori dallo schermo in laterale smetto di disegnare
+			{ // col + y sarebbe per controllare se posso disegnare la colonna corrente (se inizio a disegnare a 60 per 7 righe e arrivo al ciclio in cui voglio disegnare alla colonna 60 + 5 non posso perche ne ho solo 64
+				break; //break e non return perchè posso comunque andare in basso
+			}
+
+			std::cout << "Sprite: " << std::hex << static_cast<int>(spriteByte) << std::endl;
+			//0x80 == 1000 0000
+			uint8_t spritePixel = spriteByte & (0x80 >> col); // controllo ogni cazzo di bit da sinistra verso destra per vedere se sono accessi e prendo il bit 1 di 0x80 e lo sposto per controllare gli altri in successione nel for
+
+			std::cout << "Sprite Pixel: " << std::hex << static_cast<int>(spritePixel) << std::endl;
+			//display salva lo schermo in 64x32 non scalato
+			uint32_t *displayPixel = &display[(y + row) * 64 + (col+x)]; //prendo il pixel di display per vedere più avanti il suo stato
+
+
+			int pixelPosition = (y + row) * 64 + (col + x);
+
+
+			std::cout << "Stampo al Pixel: " << std::dec << pixelPosition << std::endl;
+			std::cout << "Display Pixel: " << std::hex << static_cast<int>(*displayPixel) << std::endl;
+
+			//POI LO FACCIO
+			//sta cosa è confusionaria un sacco e sono abbastanza ritardato ma non c'ho assolutamente voglia di cambiarla cazzi tua 
+			bool white{ false }; //da salvare per vedere di che cazzo di colore devo mettere lo schermo 
+			//è al contrario quindi white = vuoto = false e nero = riempito = true per evitare confusioni (è comunque confusionario)
+			
+			if (spritePixel != 0) //se il pixel dello sprite è 1 
+			{
+				if (*displayPixel != 0) //se il pixel del display è pieno cambio il valore di "white" in riempito che sarebbe true
+				{
+					white = true; //bianco è vero perchè immagino nero su bianco qua (anche se lo schermo è bianco su nero) (un pò complicato ma sticazzi)
+					VF = 1;
+				}
+				//accendo in entrambi i casi quindi basta una sola volta
+				//commento inutile sopra comunque 
+				//è uno XOR bit a bit e lo faccio solo con il bit selezionato
+				*displayPixel ^= 0x1;
+
+				drawScaledPixel(x*10 + col*10 , y*10 + row*10, white);
+			}
+		}
+	}
+
+	//stampo su console per vedere se funziona, se non funziona piango
+	for (int i = 0; i < 32; i++)
+	{
+		for (int j = 0; j < 64; j++)
+		{
+			if (display[i * 64 + j] == 1)
+			{
+				std::cout << "1";
+			}
+			else {
+				std::cout << "0";
+			}
+		}
+		std::cout << std::endl;
+	}
+	return VF;
+}
